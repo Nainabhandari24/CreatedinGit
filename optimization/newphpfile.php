622770cae09c3c89f00d3a3cb8b56fdd50c18a16
Encrypted <encrypted@Encrypteds-MacBook-Air.local>
2020-09-17 17:25:39 +0530

    comit

---
+++ optimization/newphpfile.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+	<?php
+	echo "sdfsasdsdadsfshjhjkhjkjk<h3>This is the con
+          
+          
+          
+          sdfasfsadf
+          
+          
+          
+          
+          sdfasdf
+          
+          
+          
+          asdfasdfsafsafdsafsaf
+          
+          
+          safdsadftent for Layout H3 Tag</h3>dafdfs"
+		?>
+</body>
+</html>
